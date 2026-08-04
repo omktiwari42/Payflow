1fc290d1db297593a5cdef05623e782b430d12fd
Om Tiwari <omktiwari42@gmail.com>
2026-08-04 18:53:50 +0530

    Add request money API constants

--- lib/core/api/api_constants.dart
+++ lib/core/api/api_constants.dart
@@ -49,8 +49,15 @@ class ApiConstants {
   static const String transactions = "/transactions";
   static const String transactionHistory = "/transactions/history";
   static const String transactionDetails = "/transactions/details";
+
   static const String sendMoney = "/transactions/send";
+
   static const String requestMoney = "/transactions/request";
+  static const String requestHistory = "/transactions/request/history";
+  static const String requestDetails = "/transactions/request/details";
+  static const String acceptRequest = "/transactions/request/accept";
+  static const String rejectRequest = "/transactions/request/reject";
+
   static const String filterTransactions = "/transactions/filter";
 
   // ==========================
